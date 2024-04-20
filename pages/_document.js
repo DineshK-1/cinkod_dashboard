a6be50cadd7e4f5be3d40765a43af6cbbc0d768c
Mayank Roy <roy050703@gmail.com>
2024-04-20 15:03:36 +0530

    added responsive navbar

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+          />
+        </Head>
+        <body className={inter.className}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
