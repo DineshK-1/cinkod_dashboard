9f611aa95b6b9d2ad2e160a2338e27bac2875bb5
Dinesh <dineshkumar.software@gmail.com>
2024-04-30 12:07:58 +0530

    Refactor code to remove commented out code and unused imports

--- services/auth.ts
+++ services/auth.ts
@@ -1,3 +1,6 @@
+import { CollegeAdmin } from "@/@types";
+import { FirebaseAdminAuth } from "@/firebase/firebaseAdmin";
+import { PrismaClient } from "@prisma/client";
 import axios from "axios";
 
 export const updateUserAuthToken = (token: string): Promise<void> => {
@@ -14,3 +17,28 @@ export const updateUserAuthToken = (token: string): Promise<void> => {
     }
   });
 };
+
+export const fetchAdminDetails = async (token: string): Promise<CollegeAdmin> => {
+
+  const { uid } = await FirebaseAdminAuth.verifyIdToken(token);
+
+  if (!uid) {
+    throw new Error("User not found!");
+  }
+  const prisma = new PrismaClient();
+  const user = await prisma.collegeAdmin.findFirst({
+    where: {
+      google_uid: uid
+    },
+    include: {
+      college: true
+    }
+  });
+  console.log(user)
+
+  if (!user) {
+    throw new Error("User not found!");
+  }
+
+  return user;
+}
